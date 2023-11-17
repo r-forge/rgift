8ccd38f213eb56e8d41a2610af43efc6738e15ca
virgil (Virgilio Gómez Rubio) <virgilio.gomez@uclm.es>
2023-11-17 12:34:28 +0000

    Encoding fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgift@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/vignettes/UTF8.R
+++ pkg/vignettes/UTF8.R
@@ -2,25 +2,36 @@
 library(RGIFT)
 
 
-Sys.setlocale(locale="es_ES.iso-8859-1")
-txt<-"\xbfEs Madrid la capital de Espa\xf1a?"
+# Get locales according to OS
+sysname <- Sys.info()['sysname']
+
+if(sysname == "Darwin") {
+  from_locale <- "es_ES.ISO8859-1"
+  to_locale <- "es_ES.UTF-8"
+} else {
+  from_locale <- "es_ES.iso-8859-1"
+  to_locale <- "es_ES.utf8"
+}
+
+Sys.setlocale(locale = from_locale)
+txt <- "\xbfEs Madrid la capital de Espa\xf1a?"
 
 #Get Local enconding:
 #Option 1: Set locale
 #locale<-"es_ES.iso-8859-15"
 #Option 2: Get locale from OS
-locale<-Sys.getlocale("LC_CTYPE")
+locale <- Sys.getlocale("LC_CTYPE")
 
-encod<-strsplit(locale, "\\.|@")[[1]][2]
+encod <- strsplit(locale, "\\.|@")[[1]][2]
 
 
-Sys.setlocale(locale="es_ES.UTF-8")
+Sys.setlocale(locale = to_locale)
 
-txt2<-iconv(txt, encod, "UTF8")
+txt2 <- iconv(txt, encod, "UTF-8")
 
 sink("utf8.txt")
 GIFTTF(txt2, TRUE)
 sink()
 
-Sys.setlocale(locale=locale)
+Sys.setlocale(locale = locale)
 
